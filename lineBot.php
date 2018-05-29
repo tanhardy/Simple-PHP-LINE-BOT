c1499e8f5452f0736372e6a0c7ad1dccda5d9191
tanhardy <39728548+tanhardy@users.noreply.github.com>
2018-05-29 18:15:49 +0700

    Update lineBot.php

--- lineBot.php
+++ lineBot.php
@@ -9,8 +9,8 @@
 	
 require_once __DIR__ . '/setting.php';
 class Linebot {
-	private $channelAccessToken;
-	private $channelSecret;
+	private $channelAccessToken;D6Z4xq2AuLVdOcFKISGs2TrweRkyGswqentG6FL/QRI9K0c7+JximL1yUM6lftv/40sMUNVyDoMB1QN+708xZOyfVsFjHH3H5dqG87PenA+sgSuOoe2orpDmf+/LyfAKaIBbttboOMJLt3ADaO/G0QdB04t89/1O/w1cDnyilFU=
+	private $channelSecret;b2a11f190f42aa183e087d6c29a6dfea
 	private $webhookResponse;
 	private $webhookEventObject;
 	private $apiReply;
