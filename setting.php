e47b1d41a68e62a29867d434101f63815febb8eb
tanhardy <39728548+tanhardy@users.noreply.github.com>
2018-05-30 12:08:29 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "D6Z4xq2AuLVdOcFKISGs2TrweRkyGswqentG6FL/QRI9K0c7+JximL1yUM6lftv/40sMUNVyDoMB1QN+708xZOyfVsFjHH3H5dqG87PenA+sgSuOoe2orpDmf+/LyfAKaIBbttboOMJLt3ADaO/G0QdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "b2a11f190f42aa183e087d6c29a6dfea";
 		return $channelSecret;
 	}
 	public function getApiReply(){
